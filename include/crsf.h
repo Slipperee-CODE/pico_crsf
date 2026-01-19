8cd7ab4f4e7f9e2d60db4f02f5523beba51fd443
slipperee_slime <scheidlercai@gmail.com>
2026-01-18 23:28:00 -0800

    crsf.h updated, uses a cool trick for proccesing entire crsf frame

--- include/crsf.h
+++ include/crsf.h
@@ -1,3 +1,8 @@
 #pragma once
 
+typedef struct __attribute((packed)){ //if you cast an address to a pointer to this type of struct, accessing struct_pointer->channel0 will access the next 11 bits starting at the address
+    unsigned channel0 : 11;
+    unsigned channel1 : 11;
+} packed_payload_t;
+ 
 void pico_crsf_is_library_accesible();
