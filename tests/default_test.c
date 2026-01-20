378bb1cb24361daa6f3da07c59ac49a6cba0c3f9
slipperee_slime <scheidlercai@gmail.com>
2026-01-19 22:02:30 -0800

    default_test.c is working

--- tests/default_test.c
+++ tests/default_test.c
@@ -7,7 +7,7 @@
 #define CRSF_UART_RX 9
 
 void on_update_rc_channels(packed_payload_t* channels){
-    printf("The value of channel 0 is %d", channels->channel0);
+    printf("The value of channel 0 is %d \n", channels->channel0);
 }
 
 int main()
@@ -17,8 +17,8 @@ int main()
     crsf_init(CRSF_UART_PORT, CRSF_UART_TX, CRSF_UART_RX, on_update_rc_channels);
     
     while (true) {
-        printf("This loop prints the value of channel0 \n");
-        pico_crsf_is_library_accesible();
+        //printf("This loop prints the value of channel0 \n");
+        //pico_crsf_is_library_accesible();
         crsf_read_incoming_frames();
     }
 }
